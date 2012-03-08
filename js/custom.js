8c818d26c4d49db368d1ff6b8a76d2e660fab168
Christopher Bishop <info@cibgraphics.com>
2012-03-08 10:16:04 -0700

    Searching for touch events

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-	// put all your jQuery goodness here
+  
+  /* Mobile touch events not binded to click but 'touchend'
+   Usage:
+   $('selector').on(TouchEndEvent, function(){
+      Code here
+     })
+  */
+  var hasTouch = "ontouchend" in document,
+      touchEndEvent = "touchend";
+      
+  if(!hasTouch) {
+    touchEndEvent = "mouseUp";
+  }
+  
+	// Put all your jQuery goodness here
+	
 });
